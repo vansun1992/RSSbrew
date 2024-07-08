c890252d6c98a2ec7c549243901fb1a7cef26ea7
vansun1992 <56790653+vansun1992@users.noreply.github.com>
2024-07-08 23:19:59 +0800

    Update settings.py

--- rssbrew/settings.py
+++ rssbrew/settings.py
@@ -18,6 +18,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOG_FILE_PATH = os.getenv('LOG_FILE_PATH', '/app/logs/feed_processing.log')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
